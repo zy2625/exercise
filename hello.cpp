e522177a064065f38fa3059e8cffb02525232357
zy2625 <3466748374@qq.com>
2022-03-27 16:21:41 +0800

    the max

--- hello.cpp
+++ hello.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
 using namespace std;
  
-// 变量声明
-extern int a, b;
-extern int c;
-extern float f;
+
   
 int main ()
 {
-  // 变量定义
-  int a, b;
-  int c;
-  float f;
- 
-  // 实际初始化
-  a = 10;
-  b = 20;
-  c = a + b;
- 
-  cout << c << endl ;
- 
-  f = 70.0/3.0;
-  cout << f << endl ;
+  int a, b, c, d, max ;
+  cout << "请输入两个数：";
+  cin >> a >> b >> c ;
+     max = (d=a>=b?a:b)>=c?d:c;  
+  cout << max <<endl; 
  
   return 0;
 }
