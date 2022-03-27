04a179be6249e3d283d9723ebe18a92b5b1e5981
zy2625 <3466748374@qq.com>
2022-03-27 16:50:49 +0800

    max 4

--- hello.cpp
+++ hello.cpp
@@ -5,11 +5,18 @@ using namespace std;
   
 int main ()
 {
-  int a, b, c, d, max ;
-  cout << "请输入两个数：";
-  cin >> a >> b >> c ;
-     max = (d=a>=b?a:b)>=c?d:c;  
-  cout << max <<endl; 
+  // 比较3个数的大小
+  // int a, b, c, d, max ;
+  // cout << "请输入两个数：";
+  // cin >> a >> b >> c ;
+  //    max = (d=a>=b?a:b)>=c?d:c;  
+  // cout << max <<endl; 
+  int a, b, c, d, m, n, max;
+  a = 10, b = 20, c = 30, d = 40;
+  m = a > b? a:b;
+  n = c> d?c:d;
+  max = m>n?m:n;
+  cout << max << endl; 
  
   return 0;
 }
