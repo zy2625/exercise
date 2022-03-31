a3ba352d1170e61a979c7a0b52220a46ba609367
zy2625 <3466748374@qq.com>
2022-04-01 00:49:09 +0800

    declared

---
+++ error.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<limits>
+using namespace std;
+// float area(float a,float b);
+int main()
+{
+  
+    float length,width;
+    length=10.0;
+    width=5.0;
+    float areas=area(length,width);
+    cout << areas <<endl;
+    return 0;
+}
+float area(float a,float b){
+    return a*b;
+}
