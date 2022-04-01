e51dabdab3dbda9d23769d40d94932577ede6fec
zy2625 <3466748374@qq.com>
2022-04-01 16:25:23 +0800

    rand

---
+++ rand.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+//#include<rand>
+
+using namespace std;
+
+int main(){
+    int j;
+    srand((unsigned)time(NULL));
+    // j = rand();
+    for(int i = 0; i < 10; i++){
+        j = rand();
+        cout << j << endl;
+    }
+    // cout << j << endl;
+    return 0;
+}
