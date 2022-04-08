5830f960f4c585f537b8caacf6adb827875cd8a7
zy2625 <3466748374@qq.com>
2022-04-08 22:04:04 +0800

    1

--- 123.cpp
+++ 123.cpp
@@ -11,12 +11,13 @@ using namespace std;
 using LL = long long;
 
 const int mod = 7 + 1e9;
+int f(char v);
 
 string s;
 vector<int> a;
 vector<int> b;
 
-int calc(int p) {
+int calc(int p) {                       //p为进制数；
     LL ans = 0;
     for (auto &i : a) {
         if (i >= p) return -1;
@@ -26,15 +27,22 @@ int calc(int p) {
     return ans;
 }
 
+int  f(char v) 
+    {                                    //v = i
+        if (isdigit(v)) return (v - '0'); //判断是否是十进制数
+        return (v - 'A' + 10);
+    }
 
 int main() {
     cin >> s;                            // s = '12'   
-        auto f = [](char v) {
-        if (isdigit(v)) return v - '0'; //判断是否是数字
-        return v - 'A' + 10;            //否则字母转为数字，返回数字给 f;
-    };
+    //     auto f = [](char v) {           //v = i
+    //     if (isdigit(v)) return v - '0'; //判断是否是十进制数
+    //     return v - 'A' + 10;            //否则字母转为数字，返回数字给 f;
+    // };
+
+    
     for (auto & i : s) {                //i = s[0],i = s[1],i = s[2]...
-        a.push_back(f(i));              //s[i] = a...;  
+        a.push_back(f(i));              //a[0] = f ,a[1] =  
     }
 
     for (int i = 2; i <= 16; ++i) {
@@ -42,8 +50,8 @@ int main() {
         if (val < 0) continue;
         b.push_back(val);
     }
-    sort(b.begin(), b.end());
-    b.erase(unique(b.begin(), b.end()), b.end());
+    sort(b.begin(), b.end());           //从小到大排序；
+    b.erase(unique(b.begin(), b.end()), b.end());       //删除
     for (auto &i : b) cout << i << endl;
     return 0;
 }
