b846a1b6f94876f65f74f99aeaae0e55e0eca242
zy2625 <3466748374@qq.com>
2022-04-11 17:31:37 +0800

    test

---
+++ 001/function.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<ctime>
+#include<stdlib.h>
+
+using namespace std;
+// int * function(){
+
+// }
+
+int * function(){
+    static int s[10];
+    srand((unsigned) time (NULL));
+    for(int i = 0; i < 10; i++){
+        s[i] = rand();
+        cout << s[i] << " " ;
+    }
+    return s;
+}
+
+int main(){
+    int *p;
+    p = function();
+    for(int i = 0; i < 10; i++ ){
+        cout << i << "  "<< p[i]<<endl;
+    }  
+
+    return 0;
+
+}
+
