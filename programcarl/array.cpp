5a88f5e9b5348887ba62d2f32f35739a79db21ad
zy2625 <3466748374@qq.com>
2022-06-12 16:27:54 +0800

    1

--- programcarl/array.cpp
+++ programcarl/array.cpp
@@ -2,3 +2,16 @@
 
 using namespace std;
 
+void text(){
+    int arr[2][3] = {
+        {0, 1, 2},
+        {3, 4, 5}
+    };
+    cout << &arr[0][0] << " " << &arr[0][1] << " " << &arr[0][2] << endl;
+    cout << &arr[1][0] << " " << &arr[1][1] << " " << &arr[1][2] << endl;
+}
+
+int main(){
+    text();
+    return 0;
+}
